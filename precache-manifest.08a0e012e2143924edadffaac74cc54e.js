358d86f487fcd3cb0e4abb79893b5db6231618c6
Serhii Shovkovuy <sergshovkanser@gmail.com>
2019-05-21 11:41:58 +0300

    first commit

---
+++ precache-manifest.08a0e012e2143924edadffaac74cc54e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "127af462e26e03e766ac",
+    "url": "/static/css/main.12ca3119.chunk.css"
+  },
+  {
+    "revision": "127af462e26e03e766ac",
+    "url": "/static/js/main.1b7deb57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fe374948b5f94d724289",
+    "url": "/static/js/2.76c86f7c.chunk.js"
+  },
+  {
+    "revision": "fd407a1b03d84564ea1ce1986465e579",
+    "url": "/static/media/death-star.fd407a1b.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA5My8zNTYvb3JpZ2luYWwvcGxhbmV0MTAuanBn.d41d8cd9.webp"
+  },
+  {
+    "revision": "e216ccad1896d44f2002b4a1a456b145",
+    "url": "/index.html"
+  }
+];
